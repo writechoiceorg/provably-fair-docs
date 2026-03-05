418f3edd956c08009771c4c6e03a26842ca87f36
Marco Colucci <marcoantoniocoluccibonfim@gmail.com>
2026-03-05 17:16:58 -0300

    sidebar initial changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,19 +83,12 @@ const config = {
         respectPrefersColorScheme: true,
       },
       navbar: {
-        title: 'My Site',
+
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/logodark.png',
         },
         items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
